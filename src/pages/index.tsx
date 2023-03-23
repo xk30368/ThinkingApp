a82648409a2a3a001a928ff9743cd64996ac7e3e
xk30368 <303681244@qq.com>
2023-03-23 18:18:31 +0800

    主页个人信息卡提交

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
-import { Button } from 'antd';
+import { Link } from 'umi';
+import { GithubOutlined, WeiboCircleOutlined, ZhihuOutlined, AlipayCircleOutlined, QqOutlined, WechatOutlined } from '@ant-design/icons';
+import imgUrl from '../assets/img/tou_x.jpg';
+import mainStyles from '../layouts/index.less';
 export default function HomePage() {
+  const svgStyle = {
+    fontSize: '1.4em',
+    // color: mainStyles.mainColor,
+  }
   return (
-    <div className="ysj-content">
-    </div>
+    <section className="ysj-content">
+      <div className="ysj-content-page1">
+        <div className="ysj-card">
+          <div className="ysj-card-imgBox">
+            <img src={imgUrl} alt="" />
+          </div>
+          <div className="ysj-card-content">
+            <div className="ysj-card-content-details">
+              <h2>云思绝<br /><span>欢迎访问云思绝个人空间</span></h2>
+              <div className="details-data">
+                <h3>234<br /><span>文章</span></h3>
+                <h3>20<br /><span>视频</span></h3>
+                <h3>14<br /><span>专栏</span></h3>
+              </div>
+              <ul className="details-link">
+                <li><Link to="/"><GithubOutlined className="icon-github" style={svgStyle} /></Link></li>
+                <li><Link to="/"><ZhihuOutlined className="icon-zh" style={svgStyle} /></Link></li>
+                <li><Link to="/"><WechatOutlined className="icon-wechat" style={svgStyle} /></Link></li>
+                <li><Link to="/"><WeiboCircleOutlined className="icon-weibo" style={svgStyle} /></Link></li>
+                <li><Link to="/"><AlipayCircleOutlined className="icon-alipay" style={svgStyle} /></Link></li>
+                <li><Link to="/"><QqOutlined className="icon-qq" style={svgStyle} /></Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ysj-content-page2">
+
+      </div>
+    </section>
   );
 }
